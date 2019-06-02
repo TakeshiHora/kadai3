25cf7c2af2d8e3e9de3f77a0a0266016bcc84397
takeshihora <a1936th@aiit.ac.jp>
2019-06-02 12:34:27 +0000

    kadai3.sh

---
+++ kadai3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def gcd(m,n):
+    x = max(m,n)
+    y = min(m,n)
+    if x%y == 0:
+        return y
+    else:
+        while x%y != 0:
+            z = x%y
+            x = y
+            y = z
+        else:
+            return z
+
+print("最大公約数は"+str(gcd(170,25))+"個")
+
